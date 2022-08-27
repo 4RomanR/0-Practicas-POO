acc2c35dce5d69b90b61137bd9da6e6aa57a82af
Roman <4rprogramming@gmail.com>
2022-08-27 08:47:25 -0500

    0

---
+++ CursoPOO/JAVA/Car.java
@@ -0,0 +1,10 @@
+class Car {
+    Integer id;
+    String license;
+    String driver;
+    Integer passenger;
+
+    void printDataCar() {
+        System.out.println("Car License:" + license + " Driver: " + driver);
+    }
+}
---
+++ Python/Account.py
@@ -0,0 +1,11 @@
+import email
+from unicodedata import name
+from xml.dom.minidom import Document
+
+
+class account:
+    id        = int
+    name      = str
+    document  = str
+    email     = str
+    password  = str
