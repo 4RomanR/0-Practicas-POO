1fdda01f3fca63992e52d42d7db3ae4bbab0c934
Roman <4rprogramming@gmail.com>
2022-08-28 08:22:48 -0500

    a

--- Javascript/Car.js
+++ Javascript/Car.js
@@ -1,6 +1,11 @@
-function (){
+function Car (license, driver){
     this.id;
     this.license;
     this.driver;
     this.passenger;
+}
+car.prototype.printDataCar = function(){
+    console.log(this.driver)
+    console.log(this.driver.name)
+    console.log(this.driver.document)
 }
--- Javascript/Payment.js
+++ Javascript/Payment.js
@@ -1,3 +1,3 @@
-function (){
+function Payment (){
     this.id;
 }
